23f978b977c128055962b8533ba3c81a4dcc0151
Robertoregalado <robertoregalado@outlook.com>
2016-06-07 20:32:08 -0300

    Ultima modificacion 7/9/2016

--- TP02/src/tp02/Televisor.java
+++ TP02/src/tp02/Televisor.java
@@ -31,6 +31,10 @@ public class Televisor extends Electrodomestico{
 	public void setPoseeHDMI(Boolean poseeHDMI) {
 		this.poseeHDMI = poseeHDMI;
 	}
+	
+	public void setPoseeHDMI(String poseeHDMI) {
+		poseeHDMI = poseeHDMI;
+	}
 
 
 }
