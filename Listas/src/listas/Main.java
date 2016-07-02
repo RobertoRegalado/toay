90555f13138810ec99c50628294c0df2fe8879fd
Xargon <Xargon@Xargon-W8>
2016-07-01 23:46:09 -0300

    OK

--- Listas/src/listas/Main.java
+++ Listas/src/listas/Main.java
@@ -3,6 +3,8 @@ package listas;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -13,121 +15,82 @@ public class Main {
 		
 	public static void main(String[] args) throws IOException 
 	{
-		Fabricante fabricante1 = new Fabricante();
-		Fabricante fabricante2 = new Fabricante();
-		Televisor tele1 = new Televisor();
-		Televisor tele2 = new Televisor();
-		AireAcondicionado aire1 = new AireAcondicionado();
-		AireAcondicionado aire2 = new AireAcondicionado();
+
+		List<Fabricante> listaF = new ArrayList<Fabricante>(); //declaro una lista de tipo Fabricante
+		List<Televisor> listaT = new ArrayList<Televisor>(); //declaro una lista de tipo Televisor
+		List<AireAcondicionado> listaA = new ArrayList<AireAcondicionado>(); //declaro una lista de tipo Televisor
 		
 		for(int i=1;i<3;i++){
 		
-			if (i==1) {
+				
+				//Fabricante
+				Fabricante FabriTemp = new Fabricante();//abro la lista y guardo en un temporal los datos
 				System.out.println("Ingrese Nombre Fabricante");
-				fabricante1.setNombre(leeteclado());
-				System.out.println("Ingrese Telefono del Fabricante "+fabricante1.getNombre());
-				fabricante1.setTelefono(leeteclado());
-				System.out.println("Ingrese Direccion del Fabricante "+fabricante1.getNombre());
-				fabricante1.setDireccion(leeteclado());
-				tele1.setFabricante(fabricante1);//asigno el fabricante 1 al televisor1
+				FabriTemp.setNombre(leeteclado());
+				System.out.println("Ingrese Telefono del Fabricante "+FabriTemp.getNombre());
+				FabriTemp.setTelefono(leeteclado());
+				System.out.println("Ingrese Direccion del Fabricante "+FabriTemp.getNombre());
+				FabriTemp.setDireccion(leeteclado());
+				
+				//Televisor
+				Televisor TeleTemp =new Televisor();//abro la lista y guardo en un temporal los datos
+				TeleTemp.setFabricante(FabriTemp);//asigno el fabricante al televisor // uso el temp de la listaF
 				System.out.println("Ingrese el modelo del Televisor "+ i);
-				tele1.setModelo(leeteclado());
+				TeleTemp.setModelo(leeteclado());
 				System.out.println("Ingrese el precio del Televisor "+ i);
-				tele1.setPrecio(Double.valueOf(Integer.valueOf(leeteclado())));//convierte el string ne double
+				TeleTemp.setPrecio(Double.valueOf(Integer.valueOf(leeteclado())));//convierte el string ne double
 				System.out.println("Ingrese la cantidad en stock del Televisor "+ i);
-				tele1.setStock(Integer.valueOf(leeteclado()));//toma solo el valor numerico y lo almacena como integer 
+				TeleTemp.setStock(Integer.valueOf(leeteclado()));//toma solo el valor numerico y lo almacena como integer 
 				System.out.println("Ingrese las Pulgadas del Televisor "+ i);
-				tele1.setPulgadas(Integer.valueOf(Integer.valueOf(leeteclado())));//convierte el string ne integer
+				TeleTemp.setPulgadas(Integer.valueOf(Integer.valueOf(leeteclado())));//convierte el string ne integer
 				System.out.println("Ingrese si el televisor posee HDMI o no Televisor "+ i);
-				tele1.setPoseeHDMI(Boolean.valueOf(leeteclado()));//toma solo el valor numerico y lo almacena como Boolean
-				aire1.setFabricante(fabricante1);
+				TeleTemp.setPoseeHDMI(Boolean.valueOf(leeteclado()));//toma solo el valor numerico y lo almacena como Boolean
+				listaT.add(TeleTemp); //pasa los datos del temporal a la lista
+				
+				//Aire
+				AireAcondicionado AireTemp = new AireAcondicionado();//abro la lista y guardo en un temporal los datos
+				AireTemp.setFabricante(FabriTemp);// uso el temp de la listaF
 				System.out.println("Ingrese el modelo del Aire Acondicionado "+ i);
-				aire1.setModelo(leeteclado());
+				AireTemp.setModelo(leeteclado());
 				System.out.println("Ingrese el precio del Aire Acondicionado "+ i);
-				aire1.setPrecio(Double.valueOf(Integer.valueOf(leeteclado())));//convierte el string ne double
+				AireTemp.setPrecio(Double.valueOf(Integer.valueOf(leeteclado())));//convierte el string ne double
 				System.out.println("Ingrese la cantidad en stock del Aire Acondicionado "+ i);
-				aire1.setStock(Integer.valueOf(leeteclado()));//toma solo el valor numerico y lo almacena como integer 
+				AireTemp.setStock(Integer.valueOf(leeteclado()));//toma solo el valor numerico y lo almacena como integer 
 				System.out.println("Ingrese las Frigorias del Aire Acondicionado "+ i);
-				aire1.setFrigorias(Integer.valueOf(Integer.valueOf(leeteclado())));
-			}
-		else{
-				System.out.println("Ingrese Nombre Fabricante");
-				fabricante2.setNombre(leeteclado());
-				System.out.println("Ingrese Telefono del Fabricante "+fabricante2.getNombre());
-				fabricante2.setTelefono(leeteclado());
-				System.out.println("Ingrese Direccion del Fabricante "+fabricante2.getNombre());
-				fabricante2.setDireccion(leeteclado());
-				tele2.setFabricante(fabricante2);
-				System.out.println("Ingrese el modelo del Televisor "+ i);
-				tele2.setModelo(leeteclado());
-				System.out.println("Ingrese el precio del Televisor "+ i);
-				tele2.setPrecio(Double.valueOf(Integer.valueOf(leeteclado())));
-				System.out.println("Ingrese la cantidad en stock del Televisor "+ i);
-				tele2.setStock(Integer.valueOf(leeteclado()));
-				System.out.println("Ingrese las Pulgadas del Televisor "+ i);
-				tele2.setPulgadas(Integer.valueOf(Integer.valueOf(leeteclado())));
-				System.out.println("Ingrese si el televisor posee HDMI o no Televisor "+ i);
-				tele2.setPoseeHDMI(Boolean.valueOf(leeteclado()));
-				aire2.setFabricante(fabricante1);
-				System.out.println("Ingrese el modelo del Aire Acondicionado "+ i);
-				aire2.setModelo(leeteclado());
-				System.out.println("Ingrese el precio del Aire Acondicionado "+ i);
-				aire2.setPrecio(Double.valueOf(Integer.valueOf(leeteclado())));
-				System.out.println("Ingrese la cantidad en stock del Aire Acondicionado "+ i);
-				aire2.setStock(Integer.valueOf(leeteclado()));
-				System.out.println("Ingrese las Frigorias del Aire Acondicionado "+ i);
-				aire2.setFrigorias(Integer.valueOf(Integer.valueOf(leeteclado())));
-			}
+				AireTemp.setFrigorias(Integer.valueOf(Integer.valueOf(leeteclado())));
+				
+				listaA.add(AireTemp); //pasa los datos del temporal a la lista
+				
+				listaF.add(FabriTemp); //pasa los datos de FabriTemp a la lista Fabricante
+		
 		}
 		
 		//muestra en pantalla lo cargado anteriormente
-		for(int i=1;i<3;i++){
-			
-			if (i==1) {
+		for(int i=0;i<2;i++){
+
 				System.out.println("");
 				System.out.println("Fabricante "+i);
-				System.out.println("Nombre: "+fabricante1.getNombre());
-				System.out.println("Telefono "+fabricante1.getTelefono());
-				System.out.println("Direccion "+fabricante1.getDireccion());
+				System.out.println("Nombre: "+listaF.get(i).getNombre());
+				System.out.println("Telefono "+listaF.get(i).getTelefono());
+				System.out.println("Direccion "+listaF.get(i).getDireccion());
 				System.out.println("");
 				System.out.println("Televisor "+i );
-				System.out.println("Modelo: "+tele1.getModelo());
-				System.out.println("Precio: "+tele1.getPrecio());
-				System.out.println("Stock: "+tele1.getStock());
-				System.out.println("Pulgadas: "+tele1.getPulgadas());
-				System.out.println("Posee HDMI: "+tele1.getPoseeHDMI());
+				System.out.println("Modelo: "+listaT.get(i).getModelo());
+				System.out.println("Precio: "+listaT.get(i).getPrecio());
+				System.out.println("Stock: "+listaT.get(i).getStock());
+				System.out.println("Pulgadas: "+listaT.get(i).getPulgadas());
+				System.out.println("Posee HDMI: "+listaT.get(i).getPoseeHDMI());
 				System.out.println("");
 				System.out.println("Aire Acondicionado "+i );
-				System.out.println("Modelo: "+aire1.getModelo());
-				System.out.println("Precio: "+aire1.getPrecio());
-				System.out.println("Stock: "+aire1.getStock());
-				System.out.println("Frigorias: "+aire1.getFrigorias());
+				System.out.println("Modelo: "+listaA.get(i).getModelo());
+				System.out.println("Precio: "+listaA.get(i).getPrecio());
+				System.out.println("Stock: "+listaA.get(i).getStock());
+				System.out.println("Frigorias: "+listaA.get(i).getFrigorias());
 			}
-		else{
-			System.out.println("");
-			System.out.println("Fabricante "+i);
-			System.out.println("Nombre: "+fabricante2.getNombre());
-			System.out.println("Telefono "+fabricante2.getTelefono());
-			System.out.println("Direccion "+fabricante2.getDireccion());
-			System.out.println("");
-			System.out.println("Televisor "+i );
-			System.out.println("Modelo: "+tele2.getModelo());
-			System.out.println("Precio: "+tele2.getPrecio());
-			System.out.println("Stock: "+tele2.getStock());
-			System.out.println("Pulgadas: "+tele2.getPulgadas());
-			System.out.println("Posee HDMI: "+tele2.getPoseeHDMI());
-			System.out.println("");
-			System.out.println("Aire Acondicionado "+i );
-			System.out.println("Modelo: "+aire2.getModelo());
-			System.out.println("Precio: "+aire2.getPrecio());
-			System.out.println("Stock: "+aire2.getStock());
-			System.out.println("Frigorias: "+aire2.getFrigorias());
-			}
-		}
-		
-		
 		
 	}
+		
+	
 		
 	private static String leeteclado() throws IOException{
 		//Captura string de teclado y lo almacena en variable
@@ -137,116 +100,3 @@ public class Main {
 	
 }
 		
-
-
-
-
-		/*Fabricante fabricante1 = new Fabricante();
-		Fabricante fabricante2 = new Fabricante();
-		Televisor tele1 = new Televisor();
-		Televisor tele2 = new Televisor();
-		AireAcondicionado aire1 = new AireAcondicionado();
-		AireAcondicionado aire2 = new AireAcondicionado();
-		
-		
-		System.out.println("Ingrese Primer Fabricante");
-		fabricante1.setNombre(entrada);
-		
-		System.out.println("Ingrese Primer Televisor");
-		tele1.setModelo(entrada);
-		tele1.setPoseeHDMI(entrada);
-		
-		
-		System.out.println("Ingrese Primer Aire Acondicionado");
-		
-		System.out.println("Ingrese Segundo Fabricante");
-		fabricante2.setNombre(entrada);
-		
-		System.out.println("Ingrese Segundo Televisor");
-		System.out.println("Ingrese Segundo Aire Acondicionado");		
-		
-
-		
-		
-		
-		
-		
-		
-		System.out.println();
-		
-		
-		
-		//nro=Integer.valueOf(entrada);//toma solo el valor numerico y lo almacena como integer si ponemos un string da error
-		System.out.println(entrada);	
-	}
-	
-	
-}
-	
-	
-	
-	
-	
-	
-	/*
-	public static void main(String[] args) throws IOException 
-	{
-		/*
-		Animal perro = new Animal("Perro");
-		//perro.especie = "Perro feo";
-		System.out.println(perro.especie);
-		System.out.println(perro.obtenerEdad());
-		perro.setearEdad("2");
-		System.out.println(perro.obtenerEdad());
-		perro.setearEdad(4);
-		System.out.println(perro.obtenerEdad());
-		
-		Persona roberto = new Persona();
-		System.out.println(roberto.nombre);
-		*/
-		
-		/*
-		  Autos auto1 = new Autos();
-		 
-		auto1.setMarca("Fiat");
-		auto1.setAnio(1996);
-		auto1.setModelo("Uno");
-		auto1.setKilometraje(200000);
-		auto1.setNroChasis("AS1");
-		auto1.setNroMotor("R02");
-		
-		System.out.println(auto1.getMarca());
-		System.out.println(auto1.getModelo());
-		System.out.println(auto1.getAnio());
-		System.out.println(auto1.getKilometraje());
-		System.out.println(auto1.mostrarDetalle());
-		System.out.println(auto1);
-		System.out.println(auto1.decirHola());
-		
-		
-	}	
-}		
-/*	
-		System.out.println("Hello world!");
-		String cadena = "Hola";
-		System.out.println(cadena);
-		
-		Integer nro = 0;
-		System.out.println(nro);
-		int nro2 = 1;
-		System.out.println(nro2);
-
-	//crea un lector de "flujo" de entrada (System.in)
-	InputStreamReader stream = new InputStreamReader(System.in);
-	
-	//Crea un Buffer (almacen) del flujo de entrada
-	BufferedReader buffer = new BufferedReader(stream);
-	
-	//leer la linea desde la consola Siempre la entrada es un String
-	String entrada = buffer.readLine();
-	nro=Integer.valueOf(entrada);//toma solo el valor numerico y lo almacena como integer si ponemos un string da error
-	System.out.println(entrada);	
-	}	
-*/
-		
-
